ace6a07ebede7583b6556c5eb8109c73ef22d9cd
Waseem Ahmad Chishti <wasimchishti87@gmail.com>
2024-01-27 13:28:41 +0000

    add back button

--- app/frontend/src/pages/layout/imgpage.tsx
+++ app/frontend/src/pages/layout/imgpage.tsx
@@ -1,20 +1,23 @@
 import styles from "./Layout.module.css";
 import architecture from "../../assets/appcomponents.png";
+import style from "react-syntax-highlighter/dist/esm/styles/hljs/a11y-dark";
 
 const Image = () => {
     return (
         <div className={styles.architecture}>
-            
-                        <img
-                            src={architecture}
-                            alt="Veraqor logo"
-                            aria-label="Architecture"
-                            // width="4000px"
-                            // height="4000px"
-                            className={styles.archimg}
-                        />
-                </div>
-                    
-  ) }
+            <img
+                src={architecture}
+                alt="Veraqor logo"
+                aria-label="Architecture"
+                // width="4000px"
+                // height="4000px"
+                className={styles.archimg}
+            />
+            <div className={styles.btn - box}>
+                <button className={style.back - btn}>Go Back</button>
+            </div>
+        </div>
+    );
+};
 
 export default Image;
